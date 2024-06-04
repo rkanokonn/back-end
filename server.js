8ee72a282aa7cdd0bd5440001a152e732e3d8d09
aunjungnueng <kanokon.r@chula.ac.th>
2024-06-04 14:59:52 +0700

    04-06-2024 [08-10]

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const hospitals = require('./routes/hospitals');
+const cookieParser = require('cookie-parser')
+const auth = require('./routes/auth')
 
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
 
- app.use(`/api/v1/hospitals` , hospitals)
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(`/api/v1/hospitals` , hospitals);
+app.use(`/api/v1/auth` , auth);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT , console.log('Server running in : ', process.env.NODE_ENV , ' mode on port  :' , PORT))
+const MONGO_URI = process.env.MONGO_URI;
+
+// ฟังก์ชันเพื่อเชื่อมต่อกับ MongoDB
+const connectDB = async () => {
+    try {
+      await mongoose.connect(MONGO_URI);
+      console.log('MongoDB Connected');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error.message);
+      process.exit(1);
+    }
+  };
+
+  connectDB();
+
+app.listen(PORT , console.log('Server running in :', process.env.NODE_ENV , ' mode on port :' , PORT))
